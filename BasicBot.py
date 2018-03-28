962ad20631cf1fb1e1a51b89de1630b3d93c96de
Mateusz Warowny <warownia1@gmail.com>
2018-03-29 00:50:45 +0100

    Add trading pokemons from box

--- BasicBot.py
+++ BasicBot.py
@@ -11,8 +11,16 @@ from collections import defaultdict, namedtuple
 
 ACCESS_TOKEN = 'NDI0NjQ1MTUzNzMxNTEwMjky.DY8JIA.MPw2ScY0FqaOfVI5RsFUYIWGWEk'
 
+# logging configuration
+log = logging.getLogger('pokebot-main')
+log.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)s %(levelname)s: %(message)s')
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
+del console_handler, formatter
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 PkmnEntry = namedtuple('PkmnEntry', 'no, name')
 client = discord.Client()
 
@@ -24,7 +32,7 @@ spawner_loop_task = None
 
 def init():
     # load pokemon list form file
-    with open('pkmn-list.txt') as f:
+    with open('pkmn-list.txt', 'r', encoding='utf8') as f:
         for line in f:
             m = re.match(r'#(\d{3}) (.+)\n', line)
             pkmn = PkmnEntry(int(m.group(1)), m.group(2))
@@ -51,9 +59,13 @@ def on_ready():
 def on_message(message):
     if message.author == client.user:
         return
-    logging.info('Message from {0.author}: {0.content}'.format(message))
+    log.info('Message from {0.author}: {0.content}'.format(message))
     if message.content.startswith('pkmn spawn'):
         yield from spawn_pkmn(message.channel, message.content[11:])
+    elif message.content == 'pkmn box':
+        yield from list_pokemon_storage(message.author, message.channel)
+    elif message.content == 'pkmn team':
+        yield from list_pokemon_team(message.author, message.channel)
     elif message.channel.is_private:
         if message.content == 'pkmn help':
             yield from print_help(message.channel)
@@ -64,10 +76,6 @@ def on_message(message):
             )
         elif message.content == 'cancel spawner':
             spawner_loop_task.cancel()
-        elif message.content == 'pkmn storage':
-            yield from list_pokemon_storage(message.author, message.channel)
-        elif message.content == 'pkmn team':
-            yield from list_pokemon_team(message.author, message.channel)
         elif message.content == 'pkmn shutdown':
             yield from client.close()
             for task in asyncio.Task.all_tasks():
@@ -88,10 +96,10 @@ def spawn_pkmn(channel, name=None):
         pkmn = next(p for p in pkmn_list if p.name == name)
     else:
         pkmn = random.choice(pkmn_list)
-    logging.info('spawning {}'.format(pkmn))
+    log.info('spawning {}'.format(pkmn))
     img_url = ('https://assets.pokemon.com/assets/cms2/'
                'img/pokedex/full/{:03}.png'.format(pkmn.no))
-    logging.debug(img_url)
+    log.debug(img_url)
     em = discord.Embed(
         title='A wild {} has appeared!'.format(pkmn.name),
         description=('Type \'catch {}\' before '
@@ -116,7 +124,7 @@ def add_caught_pokemon(user, pkmn, channel):
     text = 'Congratulations! {} caught {}.'.format(
         user.mention, pkmn.name
     )
-    if len(pkmn_team[user.id]) < 5:
+    if len(pkmn_team[user.id]) < 6:
         pkmn_team[user.id].append(pkmn.name)
     else:
         pkmn_storage[user.id].append(pkmn.name)
@@ -142,7 +150,7 @@ def list_pokemon_storage(user, channel):
 
 @asyncio.coroutine
 def list_pokemon_team(user, channel):
-    text = 'Your team:\n - ' + '\n'.join(
+    text = 'Your team:\n' + '\n'.join(
         " - **%s**" % name for name in pkmn_team[user.id]
     )
     yield from client.send_message(channel, text)
@@ -150,26 +158,52 @@ def list_pokemon_team(user, channel):
 
 @asyncio.coroutine
 def start_trade(message):
-    author = message.author
-    tokens = message.content.split()
+    seller = message.author
     buyer = message.mentions[0]
-    for i in [3, 4]:
-        pkmn_name = ' '.join(tokens[2:i])
-        print('pkmn name =', pkmn_name)
-        if pkmn_name in (pkmn.name for pkmn in pkmn_list):
-            print('pokemon exists')
-            break
-    else:
-        print('pokemon doesn\'t exist')
+    channel = message.channel
+    
+    tokens = message.content.split()
+    pkmn_name = ' '.join(tokens[3:])
+    if pkmn_name not in (pkmn.name for pkmn in pkmn_list):
         return
-    text = ("{}, {} invites you to trade and offers {}."
-            .format(buyer.mention, author.name, pkmn_name))
-    yield from client.send_message(message.channel, text)
-    check = lambda m: m.content in ['pkmn accept trade', 'pkmn cancel trade']
+        
+    # pokemon exists, send invitation to trade
+    text = ("{}, {} invites you to trade and offers {}.\n"
+            "Enter 'pkmn offer {} <pokemon>' to trade."
+            .format(buyer.mention, seller.name, pkmn_name, seller.mention))
+    yield from client.send_message(channel, text)
+
+    # wait for buyer to accept trade
+    def check(message):
+        return message.content.startswith(
+            'pkmn offer {}'.format(seller.mention)
+        )
+
     response = yield from client.wait_for_message(
-        300, channel=message.channel, author=buyer, check=check
+        300, channel=channel, author=buyer, check=check
     )
-    print(response and response.content)
+    if not response:
+        yield from client.send_message(channel, 'Trade cancelled.')
+        return
+    tokens = response.content.split()
+    pkmn2_name = ' '.join(tokens[3:])
+    if pkmn2_name not in (pkmn.name for pkmn in pkmn_list):
+        return
+    
+    log.debug('%s sells %s', seller.name, pkmn_name)
+    log.debug('%s sells %s', buyer.name, pkmn2_name)
+    
+    # trade was accepted by both sides, complete the trade
+    trade_pkmns(seller, pkmn_name, buyer, pkmn2_name)
+    yield from client.send_message(channel, 'Trade completed.')
+
+
+def trade_pkmns(seller, soffer, buyer, boffer):
+    log.info('exchange %s for %s', soffer, boffer)
+    sarg = pkmn_storage[seller.id].index(soffer)
+    barg = pkmn_storage[buyer.id].index(boffer)
+    pkmn_storage[buyer.id].append(pkmn_storage[seller.id].pop(sarg))
+    pkmn_storage[seller.id].append(pkmn_storage[buyer.id].pop(barg))
 
 
 def save_pkmn_to_file():
